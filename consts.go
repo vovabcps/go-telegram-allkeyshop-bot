0faf18b2f25133582c2fd3f9dbacc933e1deb623
vovabcps <vovkasnya@gmail.com>
2019-03-14 15:15:03 +0000

    Change found help message

--- consts.go
+++ consts.go
@@ -3,5 +3,5 @@ package main
 const (
 	welcomeMessage = "This bot searches best game deals using allkeyshop.com\n" + initialHelp
 	initialHelp    = "Type game title to start"
-	foundHelp      = "Type a number of the game to view deals. \n /cancel to go back."
+	foundHelp      = "Choose a game to view deals. \n /cancel to go back."
 )
